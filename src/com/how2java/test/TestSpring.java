7e100784caac5883469fb11bd070f6fa73b0ec92
lenovo <lenovo@DESKTOP-DOBF7UR>
2019-08-21 13:03:44 +0800

    test

---
+++ src/com/how2java/test/TestSpring.java
@@ -0,0 +1,29 @@
+package com.how2java.test;
+ 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+ 
+import com.how2java.pojo.Category;
+ 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext.xml")
+public class TestSpring {
+    @Autowired
+    Category c;
+ 
+    @Test
+    public void test(){
+        System.out.println(c.getName());
+    }
+    @Test
+    public void test1(){
+        System.out.println(c.getName());
+    }
+    @Test
+    public void test2(){
+        System.out.println(c.getName());
+    }
+}
